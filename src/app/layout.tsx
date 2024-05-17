6e03adbbaa916fc3881d4eb3314a3aa9800b07e4
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-05-17 17:26:17 +0900

    feat: footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import Providers from "./Providers";
 import GlobalStyles from "./styles/GlobalStyles";
 import Header from "@/components/organisms/Header";
+import Footer from "@/components/organisms/Footer";
 
 export default function RootLayout(props: React.PropsWithChildren){
   return(
@@ -13,6 +14,7 @@ export default function RootLayout(props: React.PropsWithChildren){
           <GlobalStyles />
           {props.children}
         </Providers>
+        <Footer />
       </body>
     </html>
   )
