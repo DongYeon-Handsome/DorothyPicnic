56a032c95079748ccfaf6dfd176d0c0f8c355070
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-05-17 13:34:32 +0900

    fix: 불필요 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Header from "@/components/organisms/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+    </>
   );
 }
