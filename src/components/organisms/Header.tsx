93a427425d8f235e31af4224d508fd2d1eb0c9e0
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-05-17 13:34:56 +0900

    feat: header 제목, 메뉴바 추가

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -15,10 +15,43 @@ const HeaderBox = styled.div`
   z-index: 9999;
 `;
 
+const VerticalLine = styled.li`
+  display: flex;
+  flex-direction: row;
+  margin-left: 3rem;
+`
+
+const Title = styled.div`
+  color: #000000;
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-decoration: none;
+  vertical-align: middle;
+  transition: 0.2s;
+  cursor: pointer;
+`
+
+const HeadLink = styled.div`
+  color: #000000;
+  font-size: 1.5rem;
+  font-weight: 600;
+  text-decoration: none;
+  vertical-align: middle;
+  transition: 0.2s;
+  margin-left: 2rem;
+  cursor: pointer;
+  :hover{ color : #F5A9A9; }
+`;
+
 export default function Header() {
   return(
     <HeaderBox>
-
+      <Title>Dorothy Picnic</Title>
+      <VerticalLine>
+        <HeadLink>Menu</HeadLink>
+        <HeadLink>Picnic</HeadLink>
+        <HeadLink>Package</HeadLink>
+      </VerticalLine>
     </HeaderBox>
   )
 }
