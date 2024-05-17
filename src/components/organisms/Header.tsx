53cd737a6f4357a65499f44e59c4e3e7db612358
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-05-17 14:27:41 +0900

    fix: Header 스타일 전면 수정

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,57 +1,52 @@
 "use client";
 import styled from 'styled-components';
 
-const HeaderBox = styled.div`
-  position: sticky;
-  top: 0;
+const HeaderBox = styled.header`
+  background-color: #ffffff;
+  color: #000000;
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  height: 3.5rem;
-  width: 100%;
-  padding: 0 4rem;
-  background-color: #FFFFFF;
-  z-index: 9999;
 `;
 
-const VerticalLine = styled.li`
-  display: flex;
-  flex-direction: row;
-  margin-left: 3rem;
-`
-
-const Title = styled.div`
-  color: #000000;
+const Logo = styled.h1`
   font-size: 1.5rem;
-  font-weight: bold;
-  text-decoration: none;
-  vertical-align: middle;
-  transition: 0.2s;
-  cursor: pointer;
-`
-
-const HeadLink = styled.div`
   color: #000000;
-  font-size: 1.5rem;
-  font-weight: 600;
-  text-decoration: none;
-  vertical-align: middle;
-  transition: 0.2s;
-  margin-left: 2rem;
   cursor: pointer;
-  :hover{ color : #F5A9A9; }
+  margin-left: 4rem;
+`;
+
+const Nav = styled.nav`
+  ul{
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin-right: 4rem;
+    li{
+      margin: 0 20px;
+      a{
+        color: #000000;
+        font-size: 1.5rem;
+        font-weight: 700;
+        text-decoration: none;
+        &:hover{
+          color: #F5A9E1;
+        }
+      }
+    }
+  }
 `;
 
 export default function Header() {
   return(
     <HeaderBox>
-      <Title>Dorothy Picnic</Title>
-      <VerticalLine>
-        <HeadLink>Menu</HeadLink>
-        <HeadLink>Picnic</HeadLink>
-        <HeadLink>Package</HeadLink>
-      </VerticalLine>
+      <Logo>Dorothy Picnic</Logo>
+      <Nav>
+        <ul>
+          <li><a href='/'>Menu</a></li>
+          <li><a href='/'>Picnic</a></li>
+        </ul>
+      </Nav>
     </HeaderBox>
   )
 }
