e50c45bbceb5929394bd75eeb279daada82600c2
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-07-16 16:04:43 +0900

    feat: 카드 공통 레이아웃 추가

---
+++ src/app/components/organisms/Menu/CardLayout.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const CardLayout = styled.div`
+  justify-content: space-between;
+  display: flex;
+  margin: auto;
+  margin-bottom: 5rem;
+  width: 80%;
+  height: auto;
+`;
+
+export default CardLayout;
