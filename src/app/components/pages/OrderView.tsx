b7a49a55446c92b3de0b6d4ee8d4f625f2b8828e
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-07-19 15:46:13 +0900

    feat: OrderView 구현 중

---
+++ src/app/components/pages/OrderView.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import styled from "styled-components";
+import ImageSlider from "../templates/home/ImageSlide";
+import MenuCard from "../mocules/MenuCard";
+import Button from "../atoms/Button";
+import Sample1 from '../../../../public/Image/Sample1.jpg';
+import Sample2 from '../../../../public/Image/Sample2.jpg';
+import Sample3 from '../../../../public/Image/Sample3.jpg';
+
+const OrderWrapper = styled.div`
+  position: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const MenuWrapper = styled.div`
+  justify-content: space-between;
+  display: flex;
+  margin: auto;
+  margin-bottom: 5rem;
+  width: 65%;
+  height: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #000000;
+  text-align: center;
+`;
+
+const OrderView: React.FC = () => {
+  const images = [
+    { src: Sample1, alt: 'Image 1' },
+    { src: Sample2, alt: 'Image 2' },
+    { src: Sample3, alt: 'Image 3' },
+  ];
+  return(
+    <OrderWrapper>
+      <ImageSlider images={images}/>
+      <Title>Menu</Title>
+      <MenuWrapper>
+        <MenuCard 
+          title="Sample 1"
+          description="description 1"
+          price="16,000won"
+          imageUrl="../../../../public/Image/Sample1.jpg"
+        />
+        <MenuCard 
+          title="Sample 2"
+          description="description 2"
+          price="16,000won"
+          imageUrl="../../../../public/Image/Sample2.jpg"
+        />
+      </MenuWrapper>
+      <Title>직접 구성을 주문해보세요</Title>
+      <Button 
+        width={150} 
+        height={80} 
+        fontSize={30}
+        href="http://pf.kakao.com/_PPcaG"
+      >
+        주문하기 카카오톡
+      </Button>
+    </OrderWrapper>
+  )
+}
+
+export default OrderView;
