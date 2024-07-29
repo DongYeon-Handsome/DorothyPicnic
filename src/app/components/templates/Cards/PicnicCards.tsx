b3fae00b1616e561ca420880b54f994e9460e35d
DongYeon-Handsome <dongyeon4367@gmail.com>
2024-07-29 09:37:11 +0900

    feat: Cards 데이터 구현

---
+++ src/app/components/templates/Cards/PicnicCards.tsx
@@ -0,0 +1,34 @@
+import Sample1 from '../../../../../public/Image/Sample1.jpg';
+import Sample2 from '../../../../../public/Image/Sample2.jpg';
+import Sample3 from '../../../../../public/Image/Sample3.jpg';
+
+const PicnicCards = [
+  { 
+    id: 1,
+    title: 'Picnic 1',
+    description: 'Picnic Description 1',
+    price: '30,000 won',
+    imageSrc: Sample1,
+    modalButtonText: '예약하기',
+    modalButtonClick: () => alert('상품이 주문되었습니다.'),
+  },
+  {
+    id: 2,
+    title: 'Picnic 2',
+    description: 'Picnic Description 2',
+    price: '40,000 won',
+    imageSrc: Sample2,
+    modalButtonText: '예약하기',
+    modalButtonClick: () => alert('상품이 주문되었습니다.'),
+  },
+  {
+    id: 3,
+    title: 'Picnic 3',
+    description: 'Picnic Description 3',
+    imageSrc: Sample3,
+    modalButtonText: '예약하기',
+    modalButtonClick: () => alert('상품이 주문되었습니다.'),
+  },
+];
+
+export default PicnicCards;
